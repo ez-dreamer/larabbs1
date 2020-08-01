7370bd4063e4aec7a908344f7e7e2282f2745127
ez-dreamer <ez_dreamer@163.com>
2020-08-01 10:27:19 +0000

    舞台布置 - 添加helpers.php - bootstra - 基础布局 - 缓存问题

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="http://weibo3.test/users/1" target="_blank">Summer</a>
+            设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+        <p class="float-right"><a href="mailto:ez-dreamer@ez_dreamer@163.com">联系我们</a></p>
+    </div>
+</footer>
